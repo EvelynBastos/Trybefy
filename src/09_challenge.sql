e13a96e93d84210f3fab8dad08f3e647bd9d73cf
EvelynBastos <lynbastos@icloud.com>
2023-11-26 18:07:06 -0300

    requisito 09

---
+++ src/09_challenge.sql
@@ -0,0 +1,9 @@
+SELECT 
+    pl.name AS 'Plano',
+    COUNT(us.full_name) AS 'Quantidade de usuários'
+FROM plans AS pl
+    INNER JOIN users AS us 
+        ON pl.id = us.plan_id
+GROUP BY pl.name
+ORDER BY 
+    pl.name;
