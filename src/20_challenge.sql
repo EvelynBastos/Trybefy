dde4e828f07a21126ec81db94b9c94875c867597
EvelynBastos <lynbastos@icloud.com>
2023-11-27 18:40:08 -0300

    requisito 20

---
+++ src/20_challenge.sql
@@ -0,0 +1,9 @@
+SELECT 
+    artists.name AS 'Artista',
+    albums.title AS 'Álbum',
+    songs.title AS 'Música'
+FROM songs
+INNER JOIN albums ON songs.album_id = albums.id
+INNER JOIN artists ON albums.artist_id = artists.id
+WHERE artists.name <> 'The Beatles'
+ORDER BY artists.name, albums.title, songs.title ASC;
